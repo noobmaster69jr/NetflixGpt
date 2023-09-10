171949304e4f3a9c27520ca9c6182e22a6d41429
noobmaster69jr <srashwin22@gmail.com>
2023-09-10 18:51:50 +0530

    added signIn, signUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-
-import './App.css'
-
+import Body from './components/Body'
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <Body/>
+      
     </>
   );
 }
